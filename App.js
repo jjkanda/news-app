40ad910102eed9e2359b9e07a47fc6ef2fbf3582
jjkanda <philososo.dubai@gmail.com>
2021-04-26 12:59:46 +0900

    componentを分割

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import ListItem from './components/ListItem';
 
 const styles = StyleSheet.create({
   container: {
@@ -36,20 +37,11 @@ const styles = StyleSheet.create({
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{ uri: 'https://picsum.photos/id/10/200/200' }}
-          />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={styles.text}>
-            「2番・指名打者」で出場した大谷は、見逃し三振、三ゴロ、中飛で迎えた第4打席で、アストロズ3番手のガルシアから2球目を中堅へ運んだ。2―2の同点での場面で放った勝ち越し本塁打は、両リーグトップに並ぶ一撃で、大谷は一塁ベースを回ると左腕を突き上げ、喜びを表現した。9回の5打席目は空振り三振に終わった。5打数1安打1打点で打率2割8分6厘。
-          </Text>
-          <Text style={styles.subText}>React News</Text>
-        </View>
-      </View>
+      <ListItem
+        author="SampleNews"
+        imageUrl="https://picsum.photos/id/10/200/200"
+        title="「2番・指名打者」で出場した大谷は、見逃し三振、三ゴロ、中飛で迎えた第4打席で、アストロズ3番手のガルシアから2球目を中堅へ運んだ。2―2の同点での場面で放った勝ち越し本塁打は、両リーグトップに並ぶ一撃で、大谷は一塁ベースを回ると左腕を突き上げ、喜びを表現した。9回の5打席目は空振り三振に終わった。5打数1安打1打点で打率2割8分6厘。"
+      />
     </View>
   );
 }
